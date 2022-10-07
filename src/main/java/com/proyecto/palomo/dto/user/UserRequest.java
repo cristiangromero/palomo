df4124278eb382caf7ddc87b91414fa5a952d459
Alben <albenbustamante@gmail.com>
2022-10-06 20:07:51 -0500

    Creado Advice para personalizar las respuestas de las excepciones

--- src/main/java/com/proyecto/palomo/dto/user/UserRequest.java
+++ src/main/java/com/proyecto/palomo/dto/user/UserRequest.java
@@ -7,6 +7,6 @@ public class UserRequest {
     private String name, username, email, password, rPassword, picture;
 
     public boolean passwordMatches() {
-        return !password.isBlank() && rPassword.equals(password);
+        return !password.isBlank() && password.equals(rPassword);
     }
 }
