81fbc15e5a0f299803fbc5432a8512f85b4fb3e3
Alben <albenbustamante@gmail.com>
2022-10-10 17:06:50 -0500

    Definido método para autenticar a un usuario

--- src/main/java/com/proyecto/palomo/controller/AuthController.java
+++ src/main/java/com/proyecto/palomo/controller/AuthController.java
@@ -1,7 +1,15 @@
 package com.proyecto.palomo.controller;
 
+import com.proyecto.palomo.dto.auth.AuthRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthenticationManager manager;
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequest authRequest) {
+        try {
+            final var authentication = manager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsernameOrEmail(), authRequest.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return ResponseEntity.ok("Sesión iniciada correctamente.");
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
 }
