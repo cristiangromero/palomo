47599d2bdac742bd47f07353b83e52a28227e7ea
Alben <albenbustamante@gmail.com>
2022-10-10 16:56:50 -0500

    Definido bean para el authentication manager

--- src/main/java/com/proyecto/palomo/config/SecurityConfig.java
+++ src/main/java/com/proyecto/palomo/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.proyecto.palomo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,11 +19,18 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
                 .antMatchers("/v2/api-docs", "/configuration/ui", "/configuration/security").permitAll()
                 .antMatchers("/swagger-ui/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .build();
     }
