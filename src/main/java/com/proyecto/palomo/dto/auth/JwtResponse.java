79c2dce2bc8e2b1f0a5361ca874a5478a7aa8828
Alben <albenbustamante@gmail.com>
2022-10-20 18:12:47 -0500

    Añadido ID del usuario al login

--- src/main/java/com/proyecto/palomo/dto/auth/JwtResponse.java
+++ src/main/java/com/proyecto/palomo/dto/auth/JwtResponse.java
@@ -8,14 +8,16 @@ import java.util.Collection;
 @Data
 public class JwtResponse {
 
+    private long userId;
     private String token;
     private String type = "Bearer";
     private String username;
     private Collection<? extends GrantedAuthority> authorities;
 
-    public JwtResponse(String accessToken, String username, Collection<? extends GrantedAuthority> authorities) {
+    public JwtResponse(long userId, String accessToken, String username, Collection<? extends GrantedAuthority> authorities) {
         this.token = accessToken;
         this.username = username;
         this.authorities = authorities;
+        this.userId = userId;
     }
 }
